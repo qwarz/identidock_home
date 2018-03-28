1e6123654ba6a4ba4482d49714aa65205c9e8914
rsv <s.rebrovskiy@gmail.com>
2018-03-28 09:10:07 +0300

    Добавлено тестирование и файл ямл для jenkins

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
 	echo "Running Development server"
 	exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+	echo "Running Unit Tests"
+	exec python "test.py"
 else
 	echo "Running Production Server"
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
